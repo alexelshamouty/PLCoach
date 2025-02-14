67efbb0a2ae31db317c50ca60e1238146c7c2445
alex.elshamouty <alex.elshamouty@gmail.com>
2025-02-14 15:03:26 +0100

    Fixing home link

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,7 +3,7 @@
       <div class="text-center">
         <img src="/logo_bg_gray_900.png" alt="Sweaty Duck Coaching" class="w-64 mx-auto">
         <div v-if="user" class="text-gray-400"> Hello {{  user?.username  }}</div>
-        <div v-if="admin" class="text-gray-400"> Check on your <NuxtLink to="/admin" class="text-white py-2" > Atheletes </NuxtLink></div>
+        <div v-if="admin" class="text-gray-400"> Check on your <NuxtLink to="/adminDashboard" class="text-white py-2" > Atheletes </NuxtLink></div>
       </div>
     </div>
 </template>
