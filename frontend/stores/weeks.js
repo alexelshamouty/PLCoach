a6aa99fa69eed637184af7fa1701b498aa524ac8
alex.elshamouty <alex.elshamouty@gmail.com>
2025-02-18 18:09:27 +0100

    Fixed nasty bug for weeks

--- frontend/stores/weeks.js
+++ frontend/stores/weeks.js
@@ -9,9 +9,24 @@ export const useWeeksStore = defineStore('weeks', {
       { id: 4, block_id: 102, label: "Week 1" },
       { id: 5, block_id: 102, label: "Week 2" },
       { id: 6, block_id: 102, label: "Week 3" },
-      { id: 7, block_id: 104, label: "Week 1" },
-      { id: 8, block_id: 104, label: "Week 2" },
-      { id: 9, block_id: 104, label: "Week 3" }
+      { id: 7, block_id: 103, label: "Week 1" },
+      { id: 8, block_id: 103, label: "Week 2" },
+      { id: 9, block_id: 103, label: "Week 3" },
+      { id: 10, block_id: 104, label: "Week 1" },
+      { id: 11, block_id: 104, label: "Week 2" },
+      { id: 12, block_id: 104, label: "Week 3" },
+      { id: 13, block_id: 105, label: "Week 1" },
+      { id: 14, block_id: 105, label: "Week 2" },
+      { id: 15, block_id: 105, label: "Week 3" },
+      { id: 16, block_id: 106, label: "Week 1" },
+      { id: 17, block_id: 106, label: "Week 2" },
+      { id: 18, block_id: 106, label: "Week 3" },
+      { id: 19, block_id: 107, label: "Week 1" },
+      { id: 20, block_id: 107, label: "Week 2" },
+      { id: 21, block_id: 107, label: "Week 3" },
+      { id: 22, block_id: 108, label: "Week 1" },
+      { id: 23, block_id: 108, label: "Week 2" },
+      { id: 24, block_id: 108, label: "Week 3" }
     ]
   }),
   actions: {
@@ -20,6 +35,8 @@ export const useWeeksStore = defineStore('weeks', {
       this.weeks.push({ id: newId, block_id, label });
     },
     getWeeksByBlockId(block_id) {
+      console.log("getWeeksByBlockId", block_id);
+      console.log(this.weeks.filter(week => week.block_id === block_id));
       return this.weeks.filter(week => week.block_id === block_id);
     }
   }
