de6b7bccaf60f9339aa701af9c67ceced6c35ebc
alex.elshamouty <alex.elshamouty@gmail.com>
2025-02-25 14:32:53 +0100

    Adjusting dynamo table

--- backend/SweatyDuckBackend/users/users.py
+++ backend/SweatyDuckBackend/users/users.py
@@ -1,5 +1,57 @@
 import json
+import boto3
+import os
+from datetime import datetime
 
 def handler(event, context):
-    print(event)
+    dynamodb = boto3.resource('dynamodb')
+    cognito = boto3.client('cognito-idp')
+    tableName = os.getenv('TABLE_NAME', 'users')
+
+    try:
+        userAttributes = event['request']['userAttributes']
+        userPoolId = event['userPoolId']
+        region = event['region']
+        userid = userAttributes['sub']
+        username = userAttributes['username']
+        email = userAttributes['email']
+        name = userAttributes['name']
+        preferred_username = userAttributes['preferred_username']
+        gender = userAttributes['gender']
+        weight = userAttributes.get('custom:weight', '0')
+        date = datetime.datetime.now().isoformat()
+        #Adding the user to the right cognito group
+        cognito.group_add_user(UserPoolId=userPoolId, Username=username, Groupname='athletes')
+
+        #Adding the user to the dynamodb table
+        #Just making sure if we get a double invocation for whatever reason..
+        table = dynamodb.Table(tableName)
+        try: 
+            item = table.get_item(Key={'Userid': userid})['Item']
+            if(item):
+                print ("User already exists")
+            else:
+                try:
+                    table.put_item(
+                        Item={
+                            'Userid': userid,
+                            'Timestamp': date,
+                            'Username': username,
+                            'Email': email,
+                            'Name': name,
+                            'Preferred_username': preferred_username,
+                            'Gender': gender,
+                            'Weight': weight
+                        }
+                    )
+                except Exception as e:
+                    print("Error while inserting user: " + e)
+                    event['response'] = 'Failed to insert user into dynamo'
+        except Exception as e:
+            print("Error while fetching user: " + e)
+            event['response'] = 'Failed to query dynamo while making sure there are no duplicates'
+    except Exception as e:
+        print ("Error + e")
+        event['response'] = f"Something else happened {e}"
+
     return event
