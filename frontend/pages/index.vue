e2463d23525a9aab8cc9c978f4bfd51c99a1a6af
alex.elshamouty <alex.elshamouty@gmail.com>
2025-03-15 11:29:08 +0100

    Fixing responsivness

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="flex justify-center items-center min-h-screen bg-gray-900">
-      <div class="text-center">
-        <div class="bg-gray-800 p-1 rounded-full">
+    <div class="flex justify-center items-center min-h-screen bg-gray-900 px-4 sm:px-0">
+      <div class="text-center w-full max-w-md">
+        <div class="bg-gray-800 p-1 rounded-full mx-auto">
           <img src="https://d2cu7vju76n2na.cloudfront.net/powerfull-duck.webp" 
                alt="Sweaty Duck Coaching" 
-               class="w-64 mx-auto rounded-full border-4 border-blue-400 shadow-lg">
+               class="w-48 sm:w-64 mx-auto rounded-full border-4 border-blue-400 shadow-lg">
         </div>
-        <div v-if="user" class="text-gray-400"> Hello {{  user?.username  }}</div>
-        <div v-if="admin" class="text-gray-400"> Check on your <NuxtLink to="/adminDashboard" class="text-white py-2" > Atheletes </NuxtLink></div>
+        <div v-if="user" class="text-gray-400 mt-4 text-lg"> Hello {{  user?.username  }}</div>
+        <div v-if="admin" class="text-gray-400 mt-2"> 
+          Check on your 
+          <NuxtLink to="/admin/adminDashboard" class="text-white py-2 hover:text-blue-400" > 
+            Athletes 
+          </NuxtLink>
+        </div>
       </div>
     </div>
 </template>
