92df41e23c8763134a2416cd96c1694b52edacea
alex.elshamouty <alex.elshamouty@gmail.com>
2025-03-15 23:12:51 +0100

    Fixes and cleanups

--- backend/trackerManagment/trackerManagement.py
+++ backend/trackerManagment/trackerManagement.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from magnum import Magnum
+from utils.response_utils import ResponseUtils
+from utils.db_utils import DBUtils
+import logging 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+@app.get("/")
+def read_root(request: Request):
+    aws_event = request.scope.get("aws.event")
+    aws_event_source = aws_event.get("source")
+    if(aws_event_source == "serverless-plugin-warmup"):
+        logger.info("WarmUp event received")
+        return {}
+
+@app.get("/trackerManagement")
+def read_trackerManagment():
+    return {"Hello": "World"}
 
 handler = Magnum(app)
